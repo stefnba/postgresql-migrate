22f4064cb6991f638279f62a1ea360f8c843a109
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-10-22 21:58:00 +0200

    test added

---
+++ tests/index.spec.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import { assert } from 'chai';
+
+import { readConfigFile } from '../src/operations';
+
+describe('Config File', () => {
+    it('should be an object', () => {
+        assert.isFunction(readConfigFile);
+    });
+    it('should return config object', () => {
+        const config = readConfigFile('app/config.json');
+        assert.isObject(config);
+    });
+});
