b55c53ab6d38e9ffdda74b3107201d561ca205a5
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-10-23 00:46:09 +0200

    config path changed

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -8,27 +8,7 @@ describe('Config File', () => {
         assert.isFunction(readConfigFile);
     });
     it('should return config object', () => {
-        const config = readConfigFile('tests/app/config.json');
-        assert.isObject(config);
-    });
-});
-
-describe('Create new Migration File', () => {
-    it('should be an object', () => {
-        assert.isFunction(readConfigFile);
-    });
-    it('should return config object', () => {
-        const config = readConfigFile('tests/app/config.json');
-        assert.isObject(config);
-    });
-});
-
-describe('Migrate Command', () => {
-    it('should be an object', () => {
-        assert.isFunction(readConfigFile);
-    });
-    it('should return config object', () => {
-        const config = readConfigFile('tests/app/config.json');
+        const config = readConfigFile('./app/config.json');
         assert.isObject(config);
     });
 });
