272508b5374df0b2eb83862db7c9a8f98616ffa6
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-10-23 16:35:10 +0200

    fix: dir structure changed

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,12 +3,14 @@ import { assert } from 'chai';
 
 import { readConfigFile } from '../src/operations';
 
+const pathToConfig = 'tests/app/db/config.json';
+
 describe('Config File', () => {
     it('should be an object', () => {
         assert.isFunction(readConfigFile);
     });
     it('should return config object', () => {
-        const config = readConfigFile('tests/app/config.json');
+        const config = readConfigFile(pathToConfig);
         assert.isObject(config);
     });
 });
