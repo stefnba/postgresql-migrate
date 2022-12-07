70e011c34288a2fc2442b24601b48d03c822db02
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-12-07 15:01:52 +0100

    fix: package.json

--- lib/operations/migrationFile.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ConfigObject } from '../types';
-/**
- * Creates new migration file in migration dir
- * @param name
- */
-declare const createMigrationFile: (name: string, config: ConfigObject) => void;
-export default createMigrationFile;
